f55147941f8aa4303343c157c10b1ea0158c0096
Molinari-Santiago <santiagomolinari07@gmail.com>
2026-05-19 08:33:20 -0300

    Actualiza configuracion del backend

--- src/app.js
+++ src/app.js
@@ -5,23 +5,19 @@ import mesasRoutes from "./routes/mesas.routes.js";
 import reservasRoutes from "./routes/reservas.routes.js";
 import productosRoutes from "./routes/productos.routes.js";
 import pedidosRoutes from "./routes/pedidos.routes.js";
-import facturasRoutes from "./routes/facturas.routes.js";
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
+app.use(cors({
+  origin: "http://localhost:3000",
+  credentials: true
+}));
 
-app.get("/", (req, res) => {
-  res.json({
-    mensaje: "API de reservas del restaurante funcionando correctamente"
-  });
-});
+app.use(express.json());
 
 app.use("/api/mesas", mesasRoutes);
 app.use("/api/reservas", reservasRoutes);
 app.use("/api/productos", productosRoutes);
 app.use("/api/pedidos", pedidosRoutes);
-app.use("/api/facturas", facturasRoutes);
 
 export default app;
